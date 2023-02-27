f45c3d4b2e8896cc95f1b59e11ead8695505056d
thomas.barbe <thomas.barbe@students.station.efrits.fr>
2023-02-27 13:15:47 +0100

    new optimised set line

--- src/set_line.c
+++ src/set_line.c
@@ -17,13 +17,16 @@ void std_set_line(t_bunny_pixelarray *px, t_bunny_position *pos, unsigned int *c
   float up;
 
   ratio = 0;
-  up = 1 / (float)(sqrt((std_abs(pos[1].x - pos[0].x) * std_abs(pos[1].x - pos[0].x)) + (std_abs(pos[1].y - pos[0].y) * std_abs(pos[1].y - pos[0].y))) + 5);
+  //up = 1 / (float)(sqrt((std_abs(pos[1].x - pos[0].x) * std_abs(pos[1].x -
+  //pos[0].x)) + (std_abs(pos[1].y - pos[0].y) * std_abs(pos[1].y - pos[0].y)))
+  //+ 5);
+  up = 1.0 / (float)pos[1].y - pos[0].y;
   while (ratio <= 1)
-    {
-      posi.x = std_get_value(ratio, pos[0].x, pos[1].x);
-      posi.y = std_get_value(ratio, pos[0].y, pos[1].y);
-      std_set_pixel(px, posi, color[0]);
-      ratio = ratio + up;
-    }
+      {
+          posi.x = pos[0].x;//std_get_value(ratio, pos[0].x, pos[1].x);
+          posi.y = std_get_value(ratio, pos[0].y, pos[1].y);
+          std_set_pixel(px, posi, color[0]);
+          ratio = ratio + up;
+      }
 }
 
